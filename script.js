63a4de42099c49783f98f1bbd396303a30f96eb8
George-25 <georgemallory26@gmail.com>
2025-02-13 22:33:26 -0500

    practice some js codes in daily coding vs code

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 //1#
-function codeEveryDay() {
-  console.log("Nyunt Sein code everyday!")
-}
+// function codeEveryDay() {
+//   console.log("Nyunt Sein code everyday!")
+// }
 
-codeEveryDay();
+// codeEveryDay();
+
+// 2.Add Two Number
+
+
+
+// store input number
+const num1 = parseInt(prompt('Enter your first number'));
+const num2 = parseInt(prompt('Enter the second number'));
+
+// add two number 
+const sum = num1 + num2;
+
+//Display the sum
+document.getElementById('result').innerHTML = `The sum of ${num1} and ${num2} is ${sum}`;
